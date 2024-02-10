972dcfd39541dcb2e801bd02d470c6c26de3346d
Anuja <anuja.ulp@gmail.com>
2024-02-10 19:43:34 +0100

    introduce routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-function App() {
+import {Routes, Route} from 'react-router-dom'
+import Layout from './pages/Layout/Layout';
+import Home from './pages/Home/Home';
 
-  return <>Main pages good</>;
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="*" element={<div>No match</div>} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
